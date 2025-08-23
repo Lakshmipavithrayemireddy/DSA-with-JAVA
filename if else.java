1d80a029f011a75f744fba77deaf2575ac85066d
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-08-23 15:30:44 +0530

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        
+        String person="boy";
+        if(person=="boy")
+        {
+            System.out.println("Go to left Counter");
+        }
+        else
+        {
+            System.out.println("Go to Right Counter");
+        }
+        System.out.println("Choosing Counters based on the Person");
+    }
+}
+
+
+
+output:
+
+Go to left Counter
+Choosing Counters based on the Person
+  
+
+  
