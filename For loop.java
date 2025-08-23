e827ab41e2ebf4dfc3ad67798a4a9e11709a6265
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-08-23 15:56:00 +0530

    Create For loop.java

---
+++ For loop.java
@@ -0,0 +1,32 @@
+class Main {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<50;i++)
+        {
+            System.out.print("Happy Birthday! ");
+            System.out.println("DSA WITH JAVA");
+        }
+        
+    }
+}
+
+output:
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
+Happy Birthday! DSA WITH JAVA
