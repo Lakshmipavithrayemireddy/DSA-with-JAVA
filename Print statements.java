5bfc2d1797f3979c5f4e31ec58105542ceac8791
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-08-23 15:18:09 +0530

    Create Print statements.java

---
+++ Print statements.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello DSA-with-JAVA!");
+        System.out.println("Hey github.com/lakshmipavithrayemireddy");
+        System.out.println("Hi Github Copilot!");
+    }
+}
+
+
+
+output:
+
+Hello DSA-with-JAVA!
+Hey github.com/lakshmipavithrayemireddy
+Hi Github Copilot!
