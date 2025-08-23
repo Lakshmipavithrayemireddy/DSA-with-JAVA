696715e877d4c55c1950f98ee56787ad37b7c4dd
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-08-23 21:50:45 +0530

    Create SQUARE PATTERN.java

---
+++ SQUARE PATTERN.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("n = ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("*");
+                if(j!=n-1)
+                {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+output:
+
+n = 8
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+*-*-*-*-*-*-*-*
+
