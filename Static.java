c491c1c3b1652630a0f86c58698c4bd8be94c055
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-09-12 22:59:01 +0530

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,17 @@
+class ChaiShop
+{
+    static String branchName= "BahubaliAdda";
+    static int cupsSold=67;
+    static int priceperCup=9;
+    
+}
+class Main {
+    public static void main(String[] args) 
+    {
+        System.out.println(ChaiShop.branchName);
+        System.out.println(ChaiShop.cupsSold);
+        System.out.println(ChaiShop.priceperCup);
+        
+       
+    }
+}
