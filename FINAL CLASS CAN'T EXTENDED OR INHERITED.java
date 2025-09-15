9f88df26464d2f060240d70945fc7c65a1e2a83f
Lakshmi Pavithra <168399713+Lakshmipavithrayemireddy@users.noreply.github.com>
2025-09-15 22:57:06 +0530

    Create FINAL CLASS CAN'T EXTENDED OR INHERITED.java

---
+++ FINAL CLASS CAN'T EXTENDED OR INHERITED.java
@@ -0,0 +1,23 @@
+final class Parent
+{
+    void display()
+    {
+        System.out.println("final method");
+    }
+}
+class Child extends Parent
+{   
+    void display()
+    {
+        System.out.println("overiding final method");
+    }
+    
+}
+class Main
+{
+    public static void main(String[] args) {
+        Child c1=new Child();
+        c1.display();
+    
+    }
+}
